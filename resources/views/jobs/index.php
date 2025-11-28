e240120b0772d33105e7e7aab477c22059921889
felix <ifyifeanyi301@gmail.com>
2025-11-28 20:25:02 +0100

    using muktiple routes

---
+++ resources/views/jobs/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Available jobs</h1>
+
+<ul>
+    <li>Web development</li>
+    <li>Software enginnering</li>
+    <li>Software</li>
+    <li>Software</li>
+</ul>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use illuminate\Http\Request;
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,22 +8,9 @@ Route::get('/', function () {
 
 // how to route in Laravel
 Route::get('/jobs', function () {
-    return 'Available jobs will be listed here.';
+    return view('jobs.index');
 })->name('jobs');
 
-//getting what request method was used
-Route::get('/test', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'ip' => $request->ip(),
-        'full' => $request->fullUrl(),
-        'headers' => $request->headers->all(),
-    ];
-});
-
-//query parameters
-Route::get('/users', function (Request $request) {
-   return $request->has('name');
-});
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
