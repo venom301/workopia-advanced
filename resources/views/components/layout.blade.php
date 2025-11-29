9be2238a5962678792ca5142353a70bb765a3572
felix <ifyifeanyi301@gmail.com>
2025-11-29 15:56:13 +0100

    adding fontawesime and starting ui design

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
     <title>Workopia | find and list jobs</title>
 </head>
-<body>
-    
-<x-header />
 
-<main>
-    <h1 class="text-3xl font-bold underline">Layout component</h1>
-    <div class="content">
-        {{ $slot }}
-    </div>
-</main>
+<body>
+
+    <x-header />
+
+    <main>
+        {{-- <h1 class="text-3xl font-bold underline">Layout component</h1> --}}
+        <div class="content">
+            {{ $slot }}
+        </div>
+    </main>
 </body>
+
 </html>
--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-<h1>Create jobs</h1>
+<x-layout>
+    <h1>Create jobs</h1>
 
-</body>
-</html>
+</x-layout>
+    
