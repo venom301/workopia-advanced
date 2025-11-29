bdf9448ad2d0a9701ee3d7ad0bf7a70360af8db5
felix <ifyifeanyi301@gmail.com>
2025-11-29 13:48:54 +0100

    creating components

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>Welcome to workopia</h1>
+    <a href="/">home</a>
+    <a href="/jobs">jobs</a>
+</div>
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-<h1>Available Jobs</h1>
+@extends('layout')
 
+@section('content')
 <ul>
    @forelse ($jobs as $job)
        <li>{{ $job }}</li>
@@ -16,6 +8,4 @@
        <li>No jobs available.</li>
     @endforelse
 </ul>
-
-</body>
-</html>
+@endsection
